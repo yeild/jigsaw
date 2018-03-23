ac688e68e0c20b5fcbb93183d74a94adaf2a84fd
huanglei <798400626@qq.com>
2018-03-23 14:02:25 +0800

    随机获取图片

--- jigsaw.js
+++ jigsaw.js
@@ -19,18 +19,17 @@
 
   function createImg(onload) {
     const img = document.createElement('img')
+    img.crossOrigin = "Anonymous"
     img.onload = onload
+    img.onerror = () => {
+      img.src = getRandomImg()
+    }
     img.src = getRandomImg()
     return img
   }
 
   function getRandomImg() {
-    return 'img.jpg'
-    if (Math.random() > 0.5) {
-      return 'img.jpg'
-    } else {
-      return 'i2.jpg'
-    }
+    return 'https://picsum.photos/300/150/?image=' + getRandomNumberByRange(0, 1084)
   }
 
   function setStyle(el, style) {
@@ -50,6 +49,7 @@
     ctx.lineTo(x + l, y + l)
     ctx.lineTo(x, y + l)
     ctx.lineTo(x, y)
+    ctx.fillStyle = '#fff'
     ctx[operation]()
     ctx.beginPath()
     ctx.arc(x, y + l / 2, r, 1.5 * PI, 0.5 * PI)
@@ -57,25 +57,30 @@
     ctx.fill()
   }
 
-  function sum (x, y) {
+  function sum(x, y) {
     return x + y
   }
-  
+
   function square(x) {
     return x * x
   }
 
   class jigsaw {
+    constructor(el, success, fail) {
+      this.el = el
+      this.success = success
+      this.fail = fail
+    }
 
-    init(element) {
-      this.initDOM(element)
+    init() {
+      this.initDOM()
       this.initImg()
       this.draw()
       this.renderStyle()
       this.bindEvents()
     }
 
-    initDOM(el) {
+    initDOM() {
       const canvas = createCanvas(w, h) // 画布
       const block = canvas.cloneNode(true) // 滑块
       const barContainer = document.createElement('div')
@@ -85,6 +90,7 @@
       const text = document.createElement('span')
       text.innerHTML = '向右滑动滑块填充拼图'
 
+      const el = this.el
       el.appendChild(canvas)
       el.appendChild(block)
       barMask.appendChild(slider)
@@ -93,7 +99,6 @@
       el.appendChild(barContainer)
 
       Object.assign(this, {
-        el,
         canvas,
         block,
         barContainer,
@@ -107,7 +112,7 @@
     }
 
     initImg() {
-      const img = createImg(() => {
+      const img = createImg((e) => {
         this.canvasCtx.drawImage(img, 0, 0, w, h)
         this.blockCtx.drawImage(img, 0, 0, w, h)
         const y = this.y - r * 2 + 2
@@ -124,6 +129,12 @@
       this.y = getRandomNumberByRange(10 + r * 2, h - (L + 10))
       draw(this.canvasCtx, 'fill', this.x, this.y)
       draw(this.blockCtx, 'clip', this.x, this.y)
+    }
+
+    clean() {
+      this.canvasCtx.clearRect(0, 0, w, h)
+      this.blockCtx.clearRect(0, 0, w, h)
+      this.block.width = w
     }
 
     renderStyle() {
@@ -159,16 +170,29 @@
     bindEvents() {
       this.el.onselectstart = () => false
 
-      let originX, originY, trail = []
+      let originX, originY, trail = [], isMouseDown = false
       this.slider.addEventListener('mousedown', function (e) {
         originX = e.x, originY = e.y
-        document.addEventListener('mousemove', drag)
+        isMouseDown = true
+      })
+      document.addEventListener('mousemove', (e) => {
+        if (!isMouseDown) return false
+        const moveX = e.x - originX
+        const moveY = e.y - originY
+        if (moveX < 0 || moveX + 38 >= w) return false
+        this.slider.style.left = moveX + 'px'
+        var blockLeft = (w - 40 - 20) / (w - 40) * moveX
+        this.block.style.left = blockLeft + 'px'
+        this.text.setAttribute('hidden', 'hidden')
+        trail.push(moveY)
       })
       document.addEventListener('mouseup', (e) => {
-        document.removeEventListener('mousemove', drag)
+        if (!isMouseDown) return false
+        isMouseDown = false
         if (e.x == originX) return false
+
         this.trail = trail
-        const { spliced, TuringTest } = this.verify()
+        const {spliced, TuringTest} = this.verify()
         if (spliced) {
           if (TuringTest) {
             this.slider.innerHTML = '√'
@@ -184,16 +208,6 @@
           this.reset()
         }
       })
-      const drag = function (e) {
-        var moveX = e.x - originX
-        var moveY = e.y - originY
-        if (moveX < 0 || moveX + 38 >= w) return false
-        this.slider.style.left = moveX + 'px'
-        var blockLeft = (w - 40 - 20) / (w - 40) * moveX
-        this.block.style.left = blockLeft + 'px'
-        this.text.setAttribute('hidden', 'hidden')
-        trail.push(moveY)
-      }.bind(this)
     }
 
     verify() {
@@ -203,7 +217,7 @@
       const stddev = Math.sqrt(deviations.map(square).reduce(sum) / arr.length) // 标准差
       const left = parseInt(this.block.style.left)
       return {
-        spliced:  Math.abs(left - this.x) < 10,
+        spliced: Math.abs(left - this.x) < 10,
         TuringTest: average !== stddev, // 只是简单的验证拖动轨迹，相等时一般为0，表示可能非人为操作
       }
     }
@@ -214,15 +228,17 @@
         this.text.removeAttribute('hidden')
         this.slider.style.left = 0
         this.block.style.left = 0
-        this.img.src = 'i2.jpg'
+        this.clean()
+        this.img.src = getRandomImg()
         this.draw()
       }, 1000)
     }
+
   }
 
   window.jigsaw = {
-    init: function (el) {
-      new jigsaw().init(el)
+    init: function (element, success, fail) {
+      new jigsaw(element, success, fail).init()
     }
   }
 }(window))
