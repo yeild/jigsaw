3d3ab49f980f6cb9c3dc3dd52218cc3d649bad81
huanglei <798400626@qq.com>
2018-08-07 16:28:23 +0800

    update style

--- jigsaw.js
+++ jigsaw.js
@@ -54,6 +54,7 @@
     ctx.lineTo(x, y + l)
     ctx.arc(x + r - 2, y + l / 2, r + 0.4, 2.76 * PI, 1.24 * PI, true)
     ctx.lineTo(x, y)
+    ctx.lineWidth = 2
     ctx.fillStyle = 'rgba(255, 255, 255, 0.7)'
     ctx.strokeStyle = 'rgba(255, 255, 255, 0.7)'
     ctx.stroke()
